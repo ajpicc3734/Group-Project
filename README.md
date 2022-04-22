7f09d7556d1504ad21af2ad80047829c2eb91b5f
ajpicc3734 <ajpicc3734@gmail.com>
2022-04-22 16:16:44 -0400

    saved song titles to local storage and updated README

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## Desctiption
 
-This application is is designed to discover trending music. The application uses the YouTube API to pull data from a new music playlist. From the playlist the user is presented with the title of the video, the video thumbnail, a link to watch the video, and a description of the video.
+This application is is designed to discover trending music. The application uses the YouTube API to pull data from a new music playlist. From the playlist the user is presented with the title of the video, the video thumbnail, a link to watch the video, and a description of the video. Due to Google security measures when the "press First & Close Pop-up" button is pressed, an error message is displayed however, once the "Press Second & Display New Music" button is pressed the music video data is displayed.
+
+The application also uses the OVH lyrics API to return song lyrics through a search function. The responsive design was developed using Materialize.
 
 ## Built With
 
@@ -13,4 +15,8 @@ This application is is designed to discover trending music. The application uses
 
 ## Website
 
+https://ajpicc3734.github.io/Group-Project/
+
 ## ScreenShot
+
+![screenshot](./css/images/screenshot.PNG)
--- js/script.js
+++ js/script.js
@@ -2,52 +2,30 @@ const youTubeApiKey = "AIzaSyB8RFLS9l9HE39KW2b9vemhPQv78ROS4uQ";
 const YoutubeClientId =
   "948793858557-vugi5gpkgek612d4ejqv2enbu3f0p8u1.apps.googleusercontent.com";
 
-<<<<<<< HEAD
-const getLyrics = async (artist, song) => {
-  const res = await fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
-  const data = await res.json();
-  const lyricinfo = document.getElementById("Lyric-search");
-  lyricinfo.innerText = data.lyrics;
-};
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const lyricsButton = document.getElementById("get-lyrics");
 
-//const getEverything = () => {
-// calls youtube
-//const youtubeData = null;
+const apiURL = "https://api.lyrics.ovh";
 
-getLyrics();
-=======
-
-  const form = document.getElementById('form');
-  const search = document.getElementById('search');
-  const result = document.getElementById('result');
-  const lyricsButton = document.getElementById('get-lyrics');
-
-const apiURL = 'https://api.lyrics.ovh';
-
-   
-
-   async function getLyrics(artist, songTitle) {
-    
-
-    fetch (`${apiURL}/v1/${artist}/${songTitle}`)
-    .then(response => {
+async function getLyrics(artist, songTitle) {
+  fetch(`${apiURL}/v1/${artist}/${songTitle}`)
+    .then((response) => {
       return response.json();
     })
-    .then(data => {
-     console.log(data)
+    .then((data) => {
+      console.log(data);
       result.innerHTML = `
       <h5><strong>${artist} - ${songTitle}</strong></h5>
       <p>${data.lyrics}</p>
       `;
     });
-    
-
 }
 
-lyricsButton.addEventListener('click', function(event) {
+lyricsButton.addEventListener("click", function (event) {
   event.preventDefault();
-  const artist = document.getElementById('artist-search').value;
-  const songTitle = document.getElementById('song-search').value;
+  const artist = document.getElementById("artist-search").value;
+  const songTitle = document.getElementById("song-search").value;
   getLyrics(artist, songTitle);
-}); 
->>>>>>> a1920f9c98ef36716f9bbead2527ff834fad8d5f
+});
