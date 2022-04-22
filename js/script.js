4054d9b7704fa0917572bac523360c1a6f7f8023
Cbannerman <cbannerman21@gmail.com>
2022-04-21 23:21:47 -0400

    fixed api to actually work, added style, steamlined ui

--- js/script.js
+++ js/script.js
@@ -4,28 +4,36 @@ const YoutubeClientId =
   "948793858557-vugi5gpkgek612d4ejqv2enbu3f0p8u1.apps.googleusercontent.com";
 
 
-  
-  const getLyrics = async (artist, song) => {
-    const res = await fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
-    const data = await res.json();
-    const lyricinfo = document.getElementById("Lyric-search");
-    lyricinfo.innerText = data.lyrics;
-  };
+  const form = document.getElementById('form');
+  const search = document.getElementById('search');
+  const result = document.getElementById('result');
+  const lyricsButton = document.getElementById('get-lyrics');
 
+const apiURL = 'https://api.lyrics.ovh';
 
-  
+   
 
-  
+   async function getLyrics(artist, songTitle) {
     
-  
 
+    fetch (`${apiURL}/v1/${artist}/${songTitle}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+     console.log(data)
+      result.innerHTML = `
+      <h5><strong>${artist} - ${songTitle}</strong></h5>
+      <p>${data.lyrics}</p>
+      `;
+    });
+    
 
-//const getEverything = () => {
-  // calls youtube
-  //const youtubeData = null;
+}
 
-  
-
-
-
-getLyrics();
+lyricsButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  const artist = document.getElementById('artist-search').value;
+  const songTitle = document.getElementById('song-search').value;
+  getLyrics(artist, songTitle);
+}); 
